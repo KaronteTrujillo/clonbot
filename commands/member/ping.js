3548977f3792bf47d94229490d5d6be4f07a8142
KaronteTrujillo <tiobarryf@gmail.com>
2025-07-16 22:59:35 +0200

    Update ping.js

--- commands/member/ping.js
+++ commands/member/ping.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'ping',
   execute: async (sock, msg) => {
     const startTime = Date.now();
-    await sock.sendMessage(msg.key.remoteJid, { react: { text: '🆗', key: msg.key } });
+    await sock.sendMessage(msg.key.remoteJid, { react: { text: 'om', key: msg.key } });
     const endTime = Date.now();
     const latency = endTime - startTime;
     const speed = latency.toFixed(2) + 'ms';
